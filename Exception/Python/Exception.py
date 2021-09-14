84a2b942aae6d79803f1acbf2072ccdd080ecebd
vladimir75vov <vladimir75vov@gmail.com>
2021-09-14 11:48:35 +0300

    Update

---
+++ Exception/Python/Exception.py
@@ -0,0 +1,15 @@
+import sys
+
+try:
+    number1 = int(input())
+    number2 = int(input())
+    print(number1/number2)
+except ValueError:
+    print("ValueError")
+except ZeroDivisionError:
+    print("ValueError")
+except Exception:
+    print("ValueError")
+
+print(sys.getsizeof(int))
+print(sys.getsizeof(float))
