8cb630f75d42bf474922b1c1be3f139a55594db1
vladimir75vov <vladimir75vov@gmail.com>
2021-09-14 09:54:20 +0300

    New Password and Updated Exception

---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static float zero(int a, int b)
+        {
+            try
+            {
+                return (float)a / b;
+            }
+            catch
+            {
+                Console.WriteLine("Делить на ноль нельзя!");
+                return 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(zero(a, b));
+
+            // int = 4 байта;
+            // float = 4 байта;
+        }
+    }
+}
---
+++ Password/Password/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibraryPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PasswordChecker passwordChecker = new PasswordChecker();
+
+            Console.WriteLine(passwordChecker.ValidatePassword("Vladimir75VoV_"));
+        }
+    }
+}
