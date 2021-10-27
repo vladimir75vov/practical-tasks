f46e862b1a4ec31b3165ab5cc385a0315f243a5c
vladimir75vov <vladimir75vov@gmail.com>
2021-10-27 09:54:11 +0300

    new opencv

--- OpenCV/Shapes/Shapes/task1/Source2.cpp
+++ OpenCV/Shapes/Shapes/task1/Source2.cpp
@@ -13,10 +13,10 @@ int main()
 	int height = 520;
 	int width = 1500;
 	Mat img(height, width, CV_8UC3);
-	Point textOrg(100, img.rows / 2);
+	Point textOrg(10, img.rows / 10);
 		int fontFace = FONT_HERSHEY_TRIPLEX;
 		double fontScale = 2;
-		Scalar color(200, 100, 50);
+		Scalar color(0, 0, 0);
 		putText(img, "smallwhitesaltfatflashlightpussy", textOrg, fontFace, fontScale, color);
 		namedWindow("Hello world", 0);
 		imshow("HelloWorld", img);
--- OpenCV/Shapes/Shapes/task2/Source1.cpp
+++ OpenCV/Shapes/Shapes/task2/Source1.cpp
@@ -26,46 +26,26 @@ int main()
 */
 Mat img;
 
-
 int main()	
 {
 	setlocale(LC_ALL, "Russian");
 	char filename[80];
-	cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã­Ã Ã§Ã¢Ã Ã­Ã¨Ã¥ Ã´Ã Ã©Ã«Ã  Ã¨ Ã­Ã Ã¦Ã¬Ã¨Ã²Ã¥ enter" << endl;
+	cout << "Ââåäèòå íàçâàíèå ôàéëà è íàæìèòå enter" << endl;
 	cout << "file.jpg" << endl;
 	cout << "sf.jpg" << endl;
-	cout << "sf2.jpg" << endl;
 	cout << "road.png" << endl;
 
 	cin.getline(filename, 80);
-	cout << "ÃŽÃ²ÃªÃ°Ã»Ã²Ã¼ Ã´Ã Ã©Ã«: ";
+	cout << "Îòêðûòü ôàéë: ";
 	cout << filename << endl;
 
 	Mat img = imread(filename, 1);
 	const char* source_window =  filename;
 
-	namedWindow(source_window, WINDOW_GUI_EXPANDED);
+	namedWindow(source_window, WINDOW_AUTOSIZE);
 	imshow(source_window, img);
-
-	Mat src_gray;
-	Mat canny_output;
-
-	cvtColor(img, src_gray, COLOR_RGB2GRAY);
-	imwrite("cvtColor.jpg", src_gray);
-	blur(src_gray, src_gray, Size(10, 10));
-	imwrite("blur.jpg", src_gray);
-
-	double otsu_tresh_val = threshold(src_gray, img, 0, 255, THRESH_BINARY | THRESH_OTSU);
-	double high_tresh_val = otsu_tresh_val, lower_tresh_val = otsu_tresh_val * 0.5;
-
-	cout << otsu_tresh_val;
-	Canny(src_gray, canny_output, lower_tresh_val, high_tresh_val, 3);
-
-	string source_grey_window = "Ã‘Ã¥Ã°Ã®Ã¥ Ã¨Ã§Ã®Ã¡Ã°Ã Ã¦Ã¥Ã­Ã¨Ã¥";
-	namedWindow(source_grey_window, WINDOW_GUI_EXPANDED); 
-	imshow(source_grey_window, canny_output);
-	imwrite("canny_output.jpg", canny_output);
 
 	waitKey(0);
 	return(0);
+
 }
