9adb35fc146e69fa4dd34226e02525a3b376ca80
vladimir75vov <vladimir75vov@gmail.com>
2021-11-24 12:21:18 +0300

    new

--- Practice/WPF/Сalculator/Сalculator/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Practice/WPF/Сalculator/Сalculator/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -253,7 +253,7 @@ namespace Сalculator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Сalculator;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Сalculator;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
